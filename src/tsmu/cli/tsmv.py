d08cbbb54116c0d606709bc20d0d903a5e1e36cc
Samat K Jain <_@skj.io>
2022-09-08 16:49:21 -0700

    ✨ feat: tsmv, blocking version of transmission-remote --verify
    transmission-remote --verify is async; you don't know when it's done, or
    whether a verify operation was successful.
    
    tsmv waits until verification is done, and it's return code indicates
    whether verification was successful. It also has a --verbose flag that
    tells you what it's doing.

---
+++ src/tsmu/cli/tsmv.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import sys
+from pathlib import Path
+from pprint import pprint  # NOQA
+
+
+import click
+
+from tsmu.util import (
+    ConnectToTransmission,
+    ParseRanges,
+    TransmissionId,
+    VerifyTorrent,
+)
+
+
+@click.command()
+@click.option("-t", "--torrent", "tid", help="transmission torrent id or infohash", required=True)
+@click.option("-v", "--verbose", default=False, is_flag=True)
+def cli(tid: str, verbose: bool = False):
+    """Verify a torrent in transmission, waiting until verification is complete."""
+    any_fail = False
+    for r in ParseRanges(tid):
+        if verbose:
+            rv = VerifyTorrent(r, statusCb=print)
+        else:
+            rv = VerifyTorrent(r)
+        if not rv:
+            any_fail = True
+    sys.exit(0) if not any_fail else sys.exit(1)
+
+
+if __name__ == "__main__":
+    cli()
---
+++ src/tsmu/util.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+
+import json
+import time
+from pathlib import Path
+from typing import Any, Callable, Generator
+
+import click
+import transmissionrpc
+
+TransmissionId = str
+
+
+def ParseRanges(s: str) -> Generator[TransmissionId, None, None]:
+    """
+    >>> list(ParseRanges("123"))
+    ['123']
+    >>> list(ParseRanges("123,347"))
+    ['123', '347']
+    >>> list(ParseRanges("123-124"))
+    ['123', '124']
+    >>> list(ParseRanges("123-124,347"))
+    ['123', '124', '347']
+    >>> list(ParseRanges("123-124, 347"))
+    ['123', '124', '347']
+    """
+    for comma_range in s.split(","):
+        comma_range = comma_range.strip()
+        if "-" not in comma_range:
+            # Could be a infohash, or transmission torrent ID
+            yield comma_range
+            continue
+
+        dash_range = comma_range.split("-")
+        assert len(dash_range) == 2
+        r1, r2 = int(dash_range[0]), int(dash_range[1]) + 1
+        for i in range(r1, r2):
+            yield str(i)
+
+
+def ConnectToTransmission() -> transmissionrpc.Client:
+    """Connect to transmission using current user's settings."""
+    settings_file_path = (
+        Path(click.get_app_dir("transmission-daemon"), "settings.json").expanduser().resolve()
+    )
+    settings = json.load(open(settings_file_path))
+
+    host, port, username, password = "localhost", settings["rpc-port"], None, None
+    tc = transmissionrpc.Client(host, port, username, password)
+    tc.timeout = 90  # Increase timeout to 90s
+    return tc
+
+
+def CheckIfDownloadDirIsCorrect(
+    tid: TransmissionId, download_dir: Path, tc: transmissionrpc.Client | None = None
+) -> bool:
+    tc = ConnectToTransmission() if not tc else tc
+    torrent = tc.get_torrent(tid)
+    if torrent.downloadDir != download_dir:
+        return False
+    return True
+
+
+def VerifyTorrent(
+    tid: TransmissionId,
+    tc: transmissionrpc.Client | None = None,
+    statusCb: Callable[str, Any] = lambda x: x,
+) -> bool:
+    tc = ConnectToTransmission() if not tc else tc
+    torrent = tc.get_torrent(tid)
+    statusCb(f'Verifying name="{torrent.name}"\n          hash="{torrent.hashString}"')
+
+    # if we haven't started verifying, don't try to start again
+    if torrent.status != "check pending":
+        tc.verify_torrent(tid)
+    while True:
+        torrent = tc.get_torrent(tid)
+        if torrent.status in ("checking", "check pending"):
+            statusCb(
+                f'Waiting to check, or check in progress. status="{torrent.status}" progress={torrent.recheckProgress}'
+            )
+            time.sleep(2)
+            continue
+
+        if torrent.error == 3:
+            statusCb(
+                f'Torrent name="{torrent.name}" hash="{torrent.hashString}" had local error={torrent.errorString}'
+            )
+            return False
+        if torrent.percentDone == 1 and torrent.haveValid and not torrent.haveUnchecked:
+            statusCb(f'Verified name="{torrent.name}" hash="{torrent.hashString}" successfully')
+            return True
+
+        statusCb(f'Unable to verify name="{torrent.name}" hash="{torrent.hashString}"')
+        return False
